37b9be77890b12cc5363dbb62f0e0b23cae91cb3
Scott Feeney <scott@oceanbase.org>
2013-11-14 19:48:45 -0500

    Add a POST route that redirects

--- src/toxiclink/routes/home.clj
+++ src/toxiclink/routes/home.clj
@@ -1,7 +1,8 @@
 (ns toxiclink.routes.home
   (:use compojure.core)
   (:require [toxiclink.views.layout :as layout]
-            [toxiclink.util :as util]))
+            [toxiclink.util :as util]
+            [ring.util.response :refer [redirect-after-post]]))
 
 (defn home-page []
   (layout/render
@@ -10,6 +11,11 @@
 (defn about-page []
   (layout/render "about.html"))
 
+(defn shorten-url [url]
+  (comment do-something-here)
+  (redirect-after-post "/")) ;; todo: make this go to url info page
+
 (defroutes home-routes
   (GET "/" [] (home-page))
-  (GET "/about" [] (about-page)))
+  (GET "/about" [] (about-page))
+  (POST "/shorten" [url] (shorten-url url)))
