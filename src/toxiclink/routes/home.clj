9e3b7c99ca720d0cb1dd98eb36fb65d67c47cdec
Scott Feeney <scott@oceanbase.org>
2013-11-18 02:30:13 -0500

    Add success page and redirector
    Hooray! The site's basic features are now complete and working.

--- src/toxiclink/routes/home.clj
+++ src/toxiclink/routes/home.clj
@@ -3,7 +3,7 @@
   (:require [toxiclink.views.layout :as layout]
             [toxiclink.util :as util]
             [toxiclink.models.db :as db]
-            [ring.util.response :refer [redirect-after-post]]))
+            [ring.util.response :refer [redirect redirect-after-post]]))
 
 (defn home-page []
   (layout/render
@@ -14,11 +14,22 @@
 
 (defn shorten-url [url]
   (let [saved-id (db/save-link [url])]
-    ;; TODO: redirect to a full confirmation page with the short link.
-    ;;       (sf 2013-11)
-    (str "We saved the link and got this back: " saved-id)))
+    (redirect-after-post (str "/confirm/" (util/id->token saved-id)))))
+
+(defn success-page [token]
+  (let [link (db/get-link (util/token->id token))]
+    (layout/render "success.html"
+                   {:input-link (:link link)
+                    :short-link (util/full-link (str "/" token))})))
+
+(defn redirect-to [token]
+  (let [link (db/get-link (util/token->id token))]
+    (redirect (:link link))))
 
 (defroutes home-routes
   (GET "/" [] (home-page))
   (GET "/about" [] (about-page))
-  (POST "/shorten" [url] (shorten-url url)))
+  (POST "/shorten" [url] (shorten-url url))
+  (GET ["/confirm/:token", :token util/token-regex] [token]
+       (success-page token))
+  (GET ["/:token", :token util/token-regex] [token] (redirect-to token)))
--- src/toxiclink/util.clj
+++ src/toxiclink/util.clj
@@ -2,9 +2,32 @@
   (:require [noir.io :as io]
             [markdown.core :as md]))
 
+(def hostname
+  (get (System/getenv) "HOSTNAME" "localhost:3000"))
+
+(def protocol
+  (get (System/getenv) "PROTOCOL" "http"))
+
+(defn full-link
+  "Given a pathname (including the starting /), returns full URL."
+  [path]
+  (str protocol "://" hostname path))
+
 (defn md->html
   "reads a markdown file from public/md and returns an HTML string"
   [filename]
   (->>
     (io/slurp-resource filename)
     (md/md-to-html-string)))
+
+(defn id->token
+  "Converts a numeric ID to the appropriate link token (string)."
+  [id]
+  (Long/toString id 36))
+
+(defn token->id
+  "Inverse of id->token."
+  [token]
+  (Long/parseLong token 36))
+
+(def token-regex #"[0-9a-zA-Z]+")
