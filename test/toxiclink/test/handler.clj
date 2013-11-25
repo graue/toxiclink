4d5e4b20a0d4271ea376d805d2b25abd6a8b5c9c
Scott Feeney <scott@oceanbase.org>
2013-11-25 04:04:15 -0500

    Replace bit-rotted test with a redirect test

--- test/toxiclink/test/handler.clj
+++ test/toxiclink/test/handler.clj
@@ -1,14 +1,23 @@
 (ns toxiclink.test.handler
   (:require [clojure.test :refer [deftest testing is]]
             [ring.mock.request :refer [request]]
-            [toxiclink.handler :as handler]))
+            [toxiclink.handler :as handler]
+            [toxiclink.routes.home :as home]
+            [toxiclink.models.db :as db]
+            [toxiclink.util :as util]))
 
 (deftest test-app
   (testing "a redirect"
-    (let [response (handler/app (request :get "/"))]
-      (is (= (:status response) 200))
-      (is (= (:body response)
-             "<html>\n    <head>\n        <title>Welcome to toxiclink</title>\n        <link href=\"/css/screen.css\" rel=\"stylesheet\" type=\"text/css\"></link>\n    </head>\n    <body>\n        <div class=\"navbar navbar-fixed-top navbar-inverse\">\n            <ul class=\"nav\">\n                <li>\n                    <a href=\"/\">Home</a>\n                </li>\n                <li>\n                    <a href=\"/about\">About</a>\n                </li>\n            </ul>\n        </div>\n        <div id=\"content\">\n        <h1>Welcome to toxiclink</h1>\n        \n<h2>Some links to get started</h2><ol><li><a href='http://www.luminusweb.net/docs/html&#95;templating.md'>HTML templating</a></li><li><a href='http://www.luminusweb.net/docs/database.md'>Accessing the database</a></li><li><a href='http://www.luminusweb.net/docs/static&#95;resources.md'>Serving static resources</a></li><li><a href='http://www.luminusweb.net/docs/responses.md'>Setting response types</a></li><li><a href='http://www.luminusweb.net/docs/routes.md'>Defining routes</a></li><li><a href='http://www.luminusweb.net/docs/middleware.md'>Adding middleware</a></li><li><a href='http://www.luminusweb.net/docs/sessions&#95;cookies.md'>Sessions and cookies</a></li><li><a href='http://www.luminusweb.net/docs/security.md'>Security</a></li><li><a href='http://www.luminusweb.net/docs/deployment.md'>Deploying the application</a></li></ol>\n\n        </div>        \n        <footer>Copyright ...</footer>\n    </body>\n</html>\n\n\n"))))
+    (with-redefs [db/get-link
+                    (fn [token]
+                      {:link ({"foo" "http://example.com/1st"
+                               "bar" "https://example.org/2nd"}
+                              token)})
+                  util/token->id identity]
+      (let [response (handler/app (request :get "/foo"))]
+        (is (= (:status response) 302))
+        (is (= ((:headers response) "Location")
+               "http://example.com/1st")))))
 
   (testing "not-found route"
     (let [response (handler/app (request :get "/invalid"))]
